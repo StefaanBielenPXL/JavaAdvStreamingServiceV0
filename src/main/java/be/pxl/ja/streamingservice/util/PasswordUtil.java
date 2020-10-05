ba0d08639f42ce1a0d4e2b066c443844cab8d083
StefaanBielenPXL <56970064+StefaanBielenPXL@users.noreply.github.com>
2020-10-05 18:25:38 +0200

    PasswordValidator

--- src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
+++ src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
@@ -3,8 +3,61 @@ package be.pxl.ja.streamingservice.util;
 public class PasswordUtil {
 
 	private static final String SPECIAL_CHARACTERS = "~!@#$%^&*()_-";
+	private static final String UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+	private static final String NUMBERS = "0123456789";
+
+	private static int score;
 
 	public static int calculateStrength(String password) {
-		return 5;
+		score = 0;
+		if (password.length() < 6) {
+			return score;
+		} else if (password.length() <= 10) {
+			score++;
+		} else {
+			score += 2;
+		}
+		hasLowerCase(password);
+		hasUpperCase(password);
+		hasNumber(password);
+		hasSpecialCharacters(password);
+		return score;
+	}
+
+	private static void hasNumber(String password) {
+		for (int i = 0; i < password.length(); i++) {
+			if (NUMBERS.contains(String.valueOf(password.charAt(i)))) {
+				score += 2;
+				return;
+			}
+		}
+	}
+
+	private static void hasLowerCase(String password) {
+		for (int i = 0; i < password.length(); i++) {
+			if (LOWER_CASE.contains(String.valueOf(password.charAt(i)))) {
+				score += 2;
+				return;
+			}
+		}
+	}
+
+	private static void hasUpperCase(String password) {
+		for (int i = 0; i < password.length(); i++) {
+			if (UPPER_CASE.contains(String.valueOf(password.charAt(i)))) {
+				score += 2;
+				return;
+			}
+		}
+	}
+
+	private static void hasSpecialCharacters(String password) {
+		for (int i = 0; i < password.length(); i++) {
+			if (SPECIAL_CHARACTERS.contains(String.valueOf(password.charAt(i)))) {
+				score += 2;
+				return;
+			}
+		}
 	}
 }
--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -1,5 +1,36 @@
 package be.pxl.ja.streamingservice.util;
 
-public class PasswordUtilCalculateStrengthTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class PasswordUtilCalculateStrengthTest {
+    @Test
+    public void scoreShouldBe0IfPasswordLengthIsLessThan6() {
+        // Arrange
+        String password = "hallo";
+        // Act
+        int result = PasswordUtil.calculateStrength(password);
+        // Assert
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void scoreShouldBe10IfPasswordIsVeryStrong() {
+        // Arrange
+        String password = "H4ll$allemaal";
+        // Act
+        int result = PasswordUtil.calculateStrength(password);
+        // Assert
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    public void scoreShouldBe3IfPasswordLengthBetween6And10WithOnlySmallCaps() {
+        // Arrange
+        String password = "allemaal";
+        // Act
+        int result = PasswordUtil.calculateStrength(password);
+        // Assert
+        Assertions.assertEquals(3, result);
+    }
 }
